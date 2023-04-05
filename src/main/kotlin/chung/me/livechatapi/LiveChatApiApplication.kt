e74f12fb5b2b2ac15fb08445313759825e9a1610
chunghyun lee <sinna94@naver.com>
2023-04-05 21:36:05 +0900

    Initial commit

---
+++ src/main/kotlin/chung/me/livechatapi/LiveChatApiApplication.kt
@@ -0,0 +1,11 @@
+package chung.me.livechatapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LiveChatApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<LiveChatApiApplication>(*args)
+}
---
+++ src/test/kotlin/chung/me/livechatapi/LiveChatApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package chung.me.livechatapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LiveChatApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
