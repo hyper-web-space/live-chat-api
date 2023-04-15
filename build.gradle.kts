d7571c32e985ce5021a89c8ce96774e68f103561
chunghyun lee <sinna94@naver.com>
2023-04-16 00:33:54 +0900

    Swagger API 문서 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,10 @@ dependencies {
   implementation("io.jsonwebtoken:jjwt-api:0.11.5")
   runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
   implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.0.2")
+  implementation("org.springdoc:springdoc-openapi-starter-common:2.0.2")
+  implementation("org.springdoc:springdoc-openapi-security:1.6.14")
 }
 
 tasks.withType<KotlinCompile> {
