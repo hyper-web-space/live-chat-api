e3e8c9705b7de79755d4f76ebc2b790e30712d5e
chunghyun lee <sinna94@naver.com>
2023-05-17 22:01:52 +0900

    pr 시 테스트 오류 수정

--- live-chat-message/build.gradle.kts
+++ live-chat-message/build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
